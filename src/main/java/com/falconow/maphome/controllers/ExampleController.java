922fef2613d85b0027ad5800c2893b19ebdf72de
Dmitry Sokolov <dmitriy88.rus@gmail.com>
2023-01-08 00:05:32 +0300

    Add notfound exeption

--- src/main/java/com/falconow/maphome/controllers/ExampleController.java
+++ src/main/java/com/falconow/maphome/controllers/ExampleController.java
@@ -2,12 +2,15 @@ package com.falconow.maphome.controllers;
 
 import com.falconow.maphome.entities.Home;
 import com.falconow.maphome.services.HomeService;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +22,7 @@ import java.util.List;
 
 
 @RestController
+@Validated
 @RequestMapping("/test")
 public class ExampleController {
 
@@ -36,15 +40,14 @@ public class ExampleController {
     }
 
     @GetMapping("/home")
-    public ResponseEntity<Home> get_home(@RequestParam(name = "street") String street, @RequestParam(name = "home") String home) {
+    public ResponseEntity<Home> get_home(@RequestParam(name = "street") @NotBlank String street, @RequestParam(name = "home") @NotBlank String home) {
         Home result = homeService.findByStreetAndHome(street, home);
-        System.out.println(result);
         if (result == null) {
-            throw new HttpStatusCodeException(HttpStatusCode.valueOf(404)) {
+            throw new HttpStatusCodeException(HttpStatus.NOT_FOUND) {
             };
         }
 
-        return new ResponseEntity<>(result, HttpStatusCode.valueOf(200));
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 
